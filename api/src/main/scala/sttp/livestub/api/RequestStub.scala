f546ffa59f4b37de3dfc86e5a9d1f886e93ab0f0
ghostbuster91 <kghost0@gmail.com>
2020-03-23 17:27:11 +0100

    Fix typo

--- api/src/main/scala/sttp/livestub/api/RequestStub.scala
+++ api/src/main/scala/sttp/livestub/api/RequestStub.scala
@@ -2,7 +2,7 @@ package sttp.livestub.api
 
 case class RequestStub(method: MethodValue, url: RequestPathAndQuery)
 object RequestStub {
-  def apply(method: MethodValue, uri: String): RequestStub = {
-    new RequestStub(method, RequestPathAndQuery.fromString(uri))
+  def apply(method: MethodValue, url: String): RequestStub = {
+    new RequestStub(method, RequestPathAndQuery.fromString(url))
   }
 }
