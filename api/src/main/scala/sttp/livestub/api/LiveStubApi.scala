3331703aa372158e54b81296ca78e6bd683be3a4
ghostbuster91 <kghost0@gmail.com>
2021-05-18 17:16:01 +0200

    Prepare minimal example

--- api/src/main/scala/sttp/livestub/api/LiveStubApi.scala
+++ api/src/main/scala/sttp/livestub/api/LiveStubApi.scala
@@ -1,8 +1,9 @@
 package sttp.livestub.api
 
 import cats.data.NonEmptyList
-import io.circe.Json
+import io.circe.{Encoder, Json}
 import sttp.model._
+import sttp.tapir.Codec.JsonCodec
 import sttp.tapir.Endpoint
 import sttp.tapir.generic.auto._
 import sttp.tapir.integ.cats.TapirCodecCats
@@ -12,37 +13,40 @@ import java.util.UUID
 import scala.collection.immutable.ListSet
 
 object LiveStubApi extends LiveStubTapirSupport with JsonSupport with TapirCodecCats {
-  val setupEndpoint: Endpoint[StubEndpointRequest, Unit, StubEndpointResponse, Any] =
-    endpoint.post
-      .in("__set")
-      .in(jsonBody[StubEndpointRequest])
-      .out(jsonBody[StubEndpointResponse])
+  implicitly[Encoder[Header]]
+//  implicitly[JsonCodec[Header]] //TODO try me!
 
-  val setupManyEndpoint: Endpoint[StubManyEndpointRequest, Unit, StubEndpointResponse, Any] =
-    endpoint.post
-      .in("__set_many")
-      .in(jsonBody[StubManyEndpointRequest])
-      .out(jsonBody[StubEndpointResponse])
-
-  val catchEndpoint: Endpoint[Request, (StatusCode, String), (StatusCode, Option[Json], List[Header]), Any] =
-    endpoint
-      .in(
-        (extractFromRequest(_.method) and paths and queryParams)
-          .map(s => Request(s._1, s._2, s._3.toMultiSeq))(r =>
-            (r.method, r.paths.map(_.path), QueryParams.fromMultiSeq(r.queries.map(q => q.key -> q.values)))
-          )
-      )
-      .out(statusCode and jsonBody[Option[Json]] and headers)
-      .errorOut(statusCode and stringBody)
-
-  val clearEndpoint: Endpoint[Unit, Unit, Unit, Any] = endpoint.post.in("__clear")
-
-  val deleteEndpoint: Endpoint[UUID, Unit, Unit, Any] = endpoint.delete
-    .in("__delete")
-    .in(path[UUID]("stubId"))
-
-  val routesEndpoint: Endpoint[Unit, Unit, StubbedRoutesResponse, Any] =
-    endpoint.get.in("__routes").out(jsonBody[StubbedRoutesResponse])
+//  val setupEndpoint: Endpoint[StubEndpointRequest, Unit, StubEndpointResponse, Any] =
+//    endpoint.post
+//      .in("__set")
+//      .in(jsonBody[StubEndpointRequest])
+//      .out(jsonBody[StubEndpointResponse])
+//
+//  val setupManyEndpoint: Endpoint[StubManyEndpointRequest, Unit, StubEndpointResponse, Any] =
+//    endpoint.post
+//      .in("__set_many")
+//      .in(jsonBody[StubManyEndpointRequest])
+//      .out(jsonBody[StubEndpointResponse])
+//
+//  val catchEndpoint: Endpoint[Request, (StatusCode, String), (StatusCode, Option[Json], List[Header]), Any] =
+//    endpoint
+//      .in(
+//        (extractFromRequest(_.method) and paths and queryParams)
+//          .map(s => Request(s._1, s._2, s._3.toMultiSeq))(r =>
+//            (r.method, r.paths.map(_.path), QueryParams.fromMultiSeq(r.queries.map(q => q.key -> q.values)))
+//          )
+//      )
+//      .out(statusCode and jsonBody[Option[Json]] and headers)
+//      .errorOut(statusCode and stringBody)
+//
+//  val clearEndpoint: Endpoint[Unit, Unit, Unit, Any] = endpoint.post.in("__clear")
+//
+//  val deleteEndpoint: Endpoint[UUID, Unit, Unit, Any] = endpoint.delete
+//    .in("__delete")
+//    .in(path[UUID]("stubId"))
+//
+//  val routesEndpoint: Endpoint[Unit, Unit, StubbedRoutesResponse, Any] =
+//    endpoint.get.in("__routes").out(jsonBody[StubbedRoutesResponse])
 }
 
 case class StubbedRoutesResponse(routes: List[StubEndpointResponse])
